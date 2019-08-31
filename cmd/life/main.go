a8d05cdc0de3071525d20afb82f11f922c5dfcf9
David W Martines <d5172@yahoo.com>
2019-08-31 08:55:48 -0500

    Change default color to green.

--- cmd/life/main.go
+++ cmd/life/main.go
@@ -14,7 +14,7 @@ func main() {
 	generations := flag.Int("gen", 100, "Number of generations.")
 	seedName := flag.String("seed", "glider", "Name of a seed to use")
 	speed := flag.Int("speed", 100, "The speed (higher=slower).")
-	colorName := flag.String("color", "cyan", "The cell color.")
+	colorName := flag.String("color", "green", "The cell color.")
 	flag.Parse()
 
 	seed := seeds.New(*seedName)
